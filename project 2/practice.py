1ee823726f90ebe39c373f2c6e65246706770de3
Yugank-yadav <luvyugank2930@gmail.com>
2025-09-01 15:34:41 +0530

    Second Project- A Tip Calculator

---
+++ project 2/practice.py
@@ -0,0 +1,15 @@
+# print("hello"[4])
+# print("hello"[-4])
+
+# print(len("12345"))
+
+# print(type(1234))
+# print(type(12.34))
+# print(type("1234"))
+# print(type(True))
+
+# Type casting
+
+# print(int("123") + int("567"))
+
+print("Number of letters in your name: " + str(len(input("Enter Your name: "))))
